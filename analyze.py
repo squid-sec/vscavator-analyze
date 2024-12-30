8d8f2852aa7c8c39c3f5184aba25f1c1dae82e1e
Nicholas Wernink <nwernink@squid-sec.com>
2024-12-29 20:59:23 -0500

    added initial shitty analysis

--- analyze.py
+++ analyze.py
@@ -1,7 +1,10 @@
 """Orchestrates the extension analysis process"""
 
 import os
+import re
+import json
 import zipfile
+from urllib.parse import urlparse
 from dotenv import load_dotenv
 from botocore.client import BaseClient
 import boto3
@@ -41,6 +44,54 @@ def unzip_vsix_file(object_key: str) -> None:
         with zipfile.ZipFile(destination_path, 'r') as zip_ref:
             zip_ref.extractall(unzip_folder)
 
+def find_package_json(object_key: str) -> dict:
+    """TODO"""
+
+    folder_path = "extensions/unzipped/" + object_key.replace("/", "-")
+    package_data = None
+
+    for root, _, files in os.walk(folder_path):
+        if "package.json" in files:
+            package_json_path = os.path.join(root, "package.json")
+            with open(package_json_path, 'r', encoding='utf-8') as f:
+                package_data = json.load(f)
+            break
+
+    return package_data
+
+def find_external_urls(object_key: str) -> list:
+    """TODO"""
+
+    url_pattern = re.compile(
+        r'https?://[^\s\"\'<>]+', # Matches URLs starting with http:// or https://
+        re.IGNORECASE
+    )
+    folder_path = "extensions/unzipped/" + object_key.replace("/", "-")
+    external_urls = set()
+
+    for root, _, files in os.walk(folder_path):
+        for file in files:
+            file_path = os.path.join(root, file)
+            if file.endswith('extension.js'):
+                with open(file_path, 'r', encoding='utf-8') as f:
+                    content = f.read()
+
+                    # Find all URLs in the file
+                    urls = url_pattern.findall(content)
+                    if urls:
+                        for url in urls:
+                            try:
+                                parsed_url = urlparse(url)
+                                base_url = f"{parsed_url.scheme}://{parsed_url.netloc}"
+                                external_urls.add(base_url.lower())
+                            except ValueError as e:
+                                print(e)
+
+    external_urls_list = list(external_urls)
+    external_urls_list.sort()
+    return external_urls_list
+
+
 def main():
     """Executes the entire extension analysis process"""
 
@@ -53,7 +104,9 @@ def main():
     for object_key in object_keys:
         get_object(s3_client, object_key)
         unzip_vsix_file(object_key)
-        break
+        object_key = "GitHub.copilot-1.254.1278"
+        package_json = find_package_json(object_key)
+        external_urls = find_external_urls(object_key)
 
 
 if __name__ == "__main__":
